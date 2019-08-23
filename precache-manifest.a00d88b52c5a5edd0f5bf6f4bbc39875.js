b0cba0fada4cc50b4aedcf5e7ce08596f8229374
Bo Amusa <Boamusa@gmail.com>
2019-08-23 00:34:04 -0400

    Updates

---
+++ precache-manifest.a00d88b52c5a5edd0f5bf6f4bbc39875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5560e2cb30c1e6ec2105ce20535a2a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "59d49a8330ebc98f25b7",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "661304d21e5d92ff38d7",
+    "url": "/robofriends/static/css/main.f14da7be.chunk.css"
+  },
+  {
+    "revision": "59d49a8330ebc98f25b7",
+    "url": "/robofriends/static/js/2.bae3d14d.chunk.js"
+  },
+  {
+    "revision": "661304d21e5d92ff38d7",
+    "url": "/robofriends/static/js/main.7d34dab4.chunk.js"
+  },
+  {
+    "revision": "fa64f21900e25e9ebf3b",
+    "url": "/robofriends/static/js/runtime~main.2c7325ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
